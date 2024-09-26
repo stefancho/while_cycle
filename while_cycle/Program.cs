37f2fe47eab1102b368684bf533d62d99eed7445
stefancho <stefanstoqnov@gmail.com>
2024-09-26 09:17:25 +0300

    Commit while cycle project

---
+++ while_cycle/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int a;
+a = 2;
+
+while ( a > 0)
+{
+    Console.WriteLine("test");
+    a--;
+}
+
+
+for (a = 1; a <= 10; a++)
+{
+    Console.WriteLine(a);
+}
+
+
+
+do 
+{
+    Console.WriteLine(a);
+
+    a++;
+
+} while (a > 0);
+
